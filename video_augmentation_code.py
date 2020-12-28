435624ed2f6408092d3d260ed965610629d275b4
Shayan Ali Bhatti <bhatt226@d.umn.edu>
2020-12-28 07:44:08 -0600

    Update video_augmentation_code.py
    Removed train path folder option, one should just point to the folder where clips are, to be augmented.

--- video_augmentation_code.py
+++ video_augmentation_code.py
@@ -77,51 +77,40 @@ if __name__=='__main__':
     print("opt \n",opt)
     main_folder_path=opt.main_folder_path
     output_folder_path=opt.output_folder_path
-    max_augmented_clips_per_video = opt.max_clips
-        
-    print("Output folder", output_folder_path)
-    print("Main folder", main_folder_path)
-    print("Max augmented clips", max_augmented_clips_per_video)
+    no_of_clips_to_augment_per_frame = opt.max_clips
+    
+    print("Output folder path", output_folder_path)
+    print("Main folder path", main_folder_path)
+    print("Max augmented clips", no_of_clips_to_augment_per_frame)
     i=1
         
     if os.path.exists(output_folder_path) and os.path.isdir(output_folder_path):
         shutil.rmtree(output_folder_path)
-    os.makedirs(output_folder_path)
-    class_names = os.listdir(main_folder_path)
-    print("Class names are ", class_names)
+    os.makedirs(output_folder_path,exist_ok=True)
     
-    # If the clips are stored according to their respective classes
-    for clas in class_names:
-        class_folder_path = f"{main_folder_path}//{clas}"
-        video_clip_names = os.listdir(class_folder_path)
-        output_class_path = output_folder_path+"//"+clas
+    video_clip_names = os.listdir(main_folder_path)
         
-        # Remove the output folder if it already exists to prevent over adding clips
-        if os.path.exists(output_class_path) and os.path.isdir(output_class_path):
-            shutil.rmtree(output_class_path)
+    print(f"Videos found are {video_clip_names}")
+    no_of_clips_available = len(video_clip_names)
 
-        print(f"Videos found are {video_clip_names}")
-        no_of_clips_available = len(video_clip_names)
+    # Run for each clip that needs to be augmented
+    for clip_no in range(no_of_clips_available):
+        # Rotate the clip based on angle range and increment the subsequent clips w.r.t. the angle increment
+        print("No. of videos to be augmented per frame", no_of_clips_to_augment_per_frame)
 
-        # Run for each clip that needs to be augmented
-        for clip_no in range(no_of_clips_available):
-            no_of_clips_to_augment_per_frame = max_augmented_clips_per_video
-            # Rotate the clip based on angle range and increment the subsequent clips w.r.t. the angle increment
-            print("No. of videos to be augmented per frame", no_of_clips_to_augment_per_frame)
+        for i in range(no_of_clips_to_augment_per_frame):
+            video_path = f"{main_folder_path}//{video_clip_names[clip_no]}"
+            # Noise value to add to videos for augmentation
+            noise_value = random.randint(0,60)
+            # Rotation angle for video augmentation
+            rotation_angle = random.randint(0,360)
 
-            for i in range(no_of_clips_to_augment_per_frame):
-                video_path = f"{class_folder_path}//{video_clip_names[clip_no]}"
-                # Noise value to add to videos for augmentation
-                noise_value = random.randint(0,60)
-                # Rotation angle for video augmentation
-                rotation_angle = random.randint(0,360)
-
-                print("Rotation angle for augmented clip is ", rotation_angle)
-                print("Noise value to add to augmented clip is ", noise_value)
-                
-                # Get fps for input video
-                fps = get_video_fps(video_path)
-                print(f"FPS of {video_clip_names[clip_no]} is {fps}")
-                # Get video reader object
-                reader = imageio.get_reader(video_path)
-                augment_and_save_frames(reader,rotation_angle,noise_value,output_class_path,video_clip_names[clip_no],i,fps)
+            print("Rotation angle for augmented clip is ", rotation_angle)
+            print("Noise value to add to augmented clip is ", noise_value)
+            
+            # Get fps for input video
+            fps = get_video_fps(video_path)
+            print(f"FPS of {video_clip_names[clip_no]} is {fps}")
+            # Get video reader object
+            reader = imageio.get_reader(video_path)
+            augment_and_save_frames(reader,rotation_angle,noise_value,output_folder_path,video_clip_names[clip_no],i,fps)
